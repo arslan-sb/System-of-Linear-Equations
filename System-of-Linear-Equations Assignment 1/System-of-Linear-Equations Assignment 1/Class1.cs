6d6cceb161814d1b502264cb1b43dd53e1a87e69
Arslan Shaukat <eskillspoint@gmail.com>
2024-03-15 06:13:02 +0500

    Threaded Application

---
+++ System-of-Linear-Equations Assignment 1/System-of-Linear-Equations Assignment 1/Class1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace System_of_Linear_Equations_Assignment_1
+{
+
+    class LinearEquationSolver
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the number of variables (N):");
+            int N = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the coefficients and constants of the equations:");
+            double[,] coefficients = new double[N, N + 1];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j <= N; j++)
+                {
+                    Console.Write($"Enter coefficient for variable x{j + 1} in equation {i + 1}: ");
+                    coefficients[i, j] = double.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\nEnter the number of iterations:");
+            int iterations = int.Parse(Console.ReadLine());
+
+            double[] result = SolveLinearEquations(coefficients, iterations);
+
+            Console.WriteLine("\nSolution:");
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine($"x{i + 1} = {result[i]}");
+            }
+        }
+
+        static double[] SolveLinearEquations(double[,] coefficients, int iterations)
+        {
+            int N = coefficients.GetLength(0);
+            double[] currentValues = new double[N];
+            double[] newValues = new double[N];
+
+            for (int iter = 0; iter < iterations; iter++)
+            {
+                Thread[] threads = CreateThreads(coefficients, currentValues, newValues, N);
+
+                // Start all threads
+                foreach (var thread in threads)
+                {
+                    thread.Start();
+                }
+
+                // Wait for all threads to finish
+                foreach (var thread in threads)
+                {
+                    thread.Join();
+                }
+
+                Array.Copy(newValues, currentValues, N);
+            }
+
+            return currentValues;
+        }
+
+        static Thread[] CreateThreads(double[,] coefficients, double[] currentValues, double[] newValues, int N)
+        {
+            Thread[] threads = new Thread[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                int index = i; // Capture the current value of i for the lambda expression
+                threads[i] = new Thread(CalculateAndUpdate(coefficients, currentValues, newValues, N, index));
+            }
+
+            return threads;
+        }
+
+        static ParameterizedThreadStart CalculateAndUpdate(double[,] coefficients, double[] currentValues, double[] newValues, int N, int index)
+        {
+            return (object obj) =>
+            {
+                double sum = coefficients[index, N];
+                for (int j = 0; j < N; j++)
+                {
+                    if (j != index)
+                    {
+                        sum -= coefficients[index, j] * currentValues[j];
+                    }
+                }
+                newValues[index] = sum / coefficients[index, index];
+            };
+        }
+    }
+
+}
+
--- System-of-Linear-Equations Assignment 1/System-of-Linear-Equations Assignment 1/Program.cs
+++ System-of-Linear-Equations Assignment 1/System-of-Linear-Equations Assignment 1/Program.cs
@@ -1,70 +1,79 @@
-﻿
+﻿//using System;
+//using System.Threading;
 
-using System;
+//class LinearEquationSolver
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("Enter the number of variables (N):");
+//        int N = int.Parse(Console.ReadLine());
 
-class LinearEquationSolver
-{
-    static void Main()
-    {
-        Console.WriteLine("Enter the number of variables (N):");
-        int N = int.Parse(Console.ReadLine());
+//        Console.WriteLine("Enter the coefficients and constants of the equations:");
+//        double[,] coefficients = new double[N, N + 1];
 
-        Console.WriteLine("Enter the coefficients and constants of the equations:");
-        double[,] coefficients = new double[N, N + 1];
+//        for (int i = 0; i < N; i++)
+//        {
+//            for (int j = 0; j <= N; j++)
+//            {
+//                Console.Write($"Enter coefficient for variable x{j + 1} in equation {i + 1}: ");
+//                coefficients[i, j] = double.Parse(Console.ReadLine());
+//            }
+//        }
 
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j <= N; j++)
-            {
-                Console.Write($"Enter coefficient for variable x{j + 1} in equation {i + 1}: ");
-                coefficients[i, j] = double.Parse(Console.ReadLine());
-            }
-        }
+//        Console.WriteLine("\nEnter the number of iterations:");
+//        int iterations = int.Parse(Console.ReadLine());
 
-        double[] result = SolveLinearEquations(coefficients);
+//        double[] result = SolveLinearEquations(coefficients, iterations);
 
-        Console.WriteLine("\nSolution:");
-        for (int i = 0; i < N; i++)
-        {
-            Console.WriteLine($"x{i + 1} = {result[i]}");
-        }
-        Console.ReadKey();
-    }
+//        Console.WriteLine("\nSolution:");
+//        for (int i = 0; i < N; i++)
+//        {
+//            Console.WriteLine($"x{i + 1} = {result[i]}");
+//        }
+//    }
 
-    static double[] SolveLinearEquations(double[,] coefficients)
-    {
-        int N = coefficients.GetLength(0);
+//    static double[] SolveLinearEquations(double[,] coefficients, int iterations)
+//    {
+//        int N = coefficients.GetLength(0);
+//        double[] currentValues = new double[N];
+//        double[] newValues = new double[N];
 
-        for (int i = 0; i < N; i++)
-        {
-            // Make the diagonal element 1
-            double divisor = coefficients[i, i];
-            for (int j = 0; j <= N; j++)
-            {
-                coefficients[i, j] /= divisor;
-            }
+//        for (int iter = 0; iter < iterations; iter++)
+//        {
+//            Thread[] threads = new Thread[N];
 
-            // Make other elements in the column zero
-            for (int k = 0; k < N; k++)
-            {
-                if (k != i)
-                {
-                    double factor = coefficients[k, i];
-                    for (int j = 0; j <= N; j++)
-                    {
-                        coefficients[k, j] -= factor * coefficients[i, j];
-                    }
-                }
-            }
-        }
+//            for (int i = 0; i < N; i++)
+//            {
+//                int index = i; // Capture the current value of i for the lambda expression
+//                threads[i] = new Thread(() =>
+//                {
+//                    double sum = coefficients[index, N];
+//                    for (int j = 0; j < N; j++)
+//                    {
+//                        if (j != index)
+//                        {
+//                            sum -= coefficients[index, j] * currentValues[j];
+//                        }
+//                    }
+//                    newValues[index] = sum / coefficients[index, index];
+//                });
+//            }
 
-        // Extract the solution
-        double[] result = new double[N];
-        for (int i = 0; i < N; i++)
-        {
-            result[i] = coefficients[i, N];
-        }
+//            // Start all threads
+//            foreach (var thread in threads)
+//            {
+//                thread.Start();
+//            }
 
-        return result;
-    }
-}
+//            // Wait for all threads to finish
+//            foreach (var thread in threads)
+//            {
+//                thread.Join();
+//            }
+
+//            Array.Copy(newValues, currentValues, N);
+//        }
+
+//        return currentValues;
+//    }
+//}
